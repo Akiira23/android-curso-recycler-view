59743fb1253a8e0105d24bdb97ee0bcb3aa86979
akiira23 <aah950@hotmail.com>
2022-05-09 09:25:29 -0300

    Adicionando notas na lista

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -1,6 +1,9 @@
 package br.com.alura.ceep.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -20,8 +23,26 @@ public class ListaNotasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
 
+        TextView botaoInsereNota = findViewById(R.id.lista_notas_insere_nota);
+        botaoInsereNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iniciaFormularioNota = new Intent(ListaNotasActivity.this,
+                        FormularioNotaActivity.class);
+                startActivity(iniciaFormularioNota);
+            }
+        });
+
         List<Nota> todasNotas = notasDeExemplo();
         configuraRecyclerView(todasNotas);
+    }
+
+    @Override
+    protected void onResume() {
+        NotaDAO dao = new NotaDAO();
+        List<Nota> todasNotas = dao.todos();
+        configuraRecyclerView(todasNotas);
+        super.onResume();
     }
 
     private List<Nota> notasDeExemplo() {
